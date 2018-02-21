c80537899b3a8f87661b4e4b265b7b431643de12
WhatAKitty <104xuqiang@163.com>
2018-02-21 16:55:25 +0800

    upgrade rn to 0.53

--- android/app/src/main/java/com/reader_x/MainApplication.java
+++ android/app/src/main/java/com/reader_x/MainApplication.java
@@ -3,6 +3,12 @@ package com.reader_x;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import io.realm.react.RealmReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.gnet.bottomsheet.RNBottomSheetPackage;
+import com.gnet.bottomsheet.RNBottomSheetPackage;
+import com.gnet.bottomsheet.RNBottomSheetPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBottomSheetPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage()
       );
